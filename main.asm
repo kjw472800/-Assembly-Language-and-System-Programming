525c5613027f64142dec37b10275c93138338509
Fang <kjw472800@gmail.com>
2018-03-10 10:41:38 +0800

    menu options finished

--- main.asm
+++ main.asm
@@ -21,8 +21,15 @@ include Macros.inc
 
 .data
 
-ONE	REAL8 1.1
-S	REAL8 1.0, 1.0, 100 DUP (?)
+menuOP BYTE "1) Show colorful frames",0dh,0ah
+		BYTE "2) Sum up signed integers",0dh,0ah
+		BYTE "3) Sum up unsigned integers",0dh,0ah
+		BYTE "4) Compute sin(x)",0dh,0ah
+		BYTE "5) Show student information",0dh,0ah
+		BYTE "6) Quit",0dh,0ah,0
+
+selectOP 	BYTE "Please select an option......",0dh,0ah,0
+
  
 .code
 
@@ -30,39 +37,26 @@ S	REAL8 1.0, 1.0, 100 DUP (?)
 ;
 ;
 main PROC
-	call GenerateNumbers	; generate numbers
-	call PrintResult		; print the result
+	call Showmenu	 
+	call Select
 	call ReadInt			; block the program
 	exit
 main ENDP
 
-GenerateNumbers PROC
-	finit				; initialize the floating point unit (FPU)				
-	mov edi, 0			; offset of the first element, 
-						; i.e., offset of S(1). Here, n = 1
-	mov ecx, 18		; loop counter
-L0:
-	finit
-	fld S[edi]			; load S(n) to FPU
-	fld S[edi+8]		; load S(n+1) to FPU
-	fadd ST(0), ST(1)	; ST(0) <- S(n+1) + S(n)
-	fst S[edi+16]		; S(n+2) <- S(n+1) + S(n)
-	add edi, 8			; set edi to the offset of the next element
-	loop L0				; ecx <- ecx -1; if ecx !=0, goto L0
-	ret					; return to the caller
-GenerateNumbers ENDP
 
-PrintResult PROC
-	mov edi, 0			; set edi to the offset of the first element
-	mov ecx, 18			; print 10 elements
-L0:
-	finit				; initialize the floating point unit
-	fld S[edi]			; load S[edi] (8 bytes) to FPU
-	call WriteFloat		; write ST(0)
-	call Crlf			; call line feed
-	add edi, 8			; set edi to the offset of the next element
-	loop L0				; ecx <- ecx -1; if ecx > 0, goto L0
-	ret					; return to the caller
-PrintResult ENDP
+Showmenu PROC
+	mov edx , OFFSET menuOP
+	call WriteString 
+	ret 
+Showmenu ENDP
+
+Select PROC
+	mov edx , OFFSET selectOP
+	call WriteString
+	call Readkey
+	call Write
+	ret
+Select ENDP
+
 
 END main
