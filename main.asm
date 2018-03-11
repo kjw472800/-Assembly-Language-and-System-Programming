db028a74ff9bf255f76412c6cec1e6dfa77f83f6
Fang <kjw472800@gmail.com>
2018-03-11 21:33:30 +0800

    Options are finished

--- main.asm
+++ main.asm
@@ -29,18 +29,24 @@ menuOP BYTE "1) Show colorful frames",0dh,0ah
 		BYTE "6) Quit",0dh,0ah,0
 
 selectOP 	BYTE "Please select an option......",0dh,0ah,0
+wrongOP 	BYTE "Please select an option between 1 to 6",0dh,0ah,0
+OP 		BYTE ?
 
- 
 .code
 
 ;
 ;
 ;
 main PROC
+
+	mov ecx , 1
+L0:
+	;call Clrscr
+	call Crlf
 	call Showmenu	 
 	call Select
-	call ReadInt			; block the program
-	exit
+    jmp L0			
+
 main ENDP
 
 
@@ -50,13 +56,86 @@ Showmenu PROC
 	ret 
 Showmenu ENDP
 
+
 Select PROC
 	mov edx , OFFSET selectOP
 	call WriteString
-	call Readkey
-	call Write
+LookForKey:
+	mov  eax,50
+	call Delay
+	call ReadKey
+	jz   LookForKey
+	call WriteChar
+	
+
+	cmp  al , '1'
+	jnz  Option2
+	call WriteChar
+	call Crlf
+	ret
+Option2: 
+	cmp  al , '2'
+	jnz  Option3
+	call WriteChar
+	call Crlf
+	ret
+Option3: 
+	cmp  al , '3'
+	jnz  Option4  
+	call WriteChar
+	call Crlf
+	ret
+Option4: 
+	cmp  al , '4'
+	jnz  Option5 
+	call WriteChar
+	call Crlf
+	ret
+Option5: 
+	cmp  al , '5'
+	jnz  Option6  
+	call WriteChar
+	call Crlf
+	ret
+Option6: 
+	cmp  al , '6'
+	exit
+Optiondefault:
+	
+	mov edx , OFFSET wrongOP
+	call WriteString
+	call Crlf
 	ret
 Select ENDP
 
+Colorframe PROC
+
+	ret 
+Colorframe ENDP
+
+Sumofsigned PROC
+
+	ret 
+Sumofsigned ENDP
+
+Sumofunsigned PROC
+
+	ret 
+Sumofunsigned ENDP
+
+Sin PROC
+
+	ret 
+Sin ENDP
+
+Showinform PROC
+
+	ret 
+Showinform ENDP
+
+Quit PROC
+
+	ret 
+Quit ENDP
 
 END main
